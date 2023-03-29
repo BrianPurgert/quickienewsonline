d59c4d4970926321a7d17a72ae75762720992c0c
Brian Purgert <brianpurgert2@gmail.com>
2023-03-29 16:34:53 -0400

    media added

--- app.vue
+++ app.vue
@@ -1,5 +1,23 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+	<div class = "d-flex align-center flex-column">
+
+
+		<v-card width = "500">
+			<v-img
+				src = "/logo.svg"
+				width = "400"
+				class = "mx-auto"
+			></v-img>
+			<v-card-title>QNO</v-card-title>
+			<v-card-subtitle>get filled in on the news in minutes</v-card-subtitle>
+			<v-card-text></v-card-text>
+		</v-card>
+		<v-btn class = "mt-10">Quickie News Online</v-btn>
+	</div>
 </template>
+
+<style lang = "scss">
+
+</style>
+<script setup lang = "ts">
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,57 @@
-export default {
-  nitro: {
-    preset: 'vercel-edge',
-  },
-};
+// @ts-ignore
+import vuetify from 'vite-plugin-vuetify'
+
+// import {createResolver} from '@nuxt/kit'
+
+// const {resolve} = createResolver(import.meta.url)
+
+export default defineNuxtConfig({
+	typescript: {shim: false},
+	build: {transpile: ['vuetify']},
+	sourcemap: {
+		client: false,
+		server: false,
+	},
+	modules: [
+		"@kevinmarrec/nuxt-pwa",
+		async (options, nuxt) => {
+			nuxt.hooks.hook("vite:extendConfig", (config) =>
+				// @ts-ignore
+				config.plugins.push(
+					vuetify({
+
+						styles: {configFile: "assets/variables.scss"},
+
+					})
+				)
+			);
+		},
+	],
+	vite: {
+		define: {
+			'process.env.DEBUG': false,
+		},
+		// css: {
+		// 	preprocessorOptions: {
+		// 		scss: {
+		// 			additionalData: `@import "assets/variables.scss";
+		//   `
+		// 		}
+		// 	}
+		// }
+	},
+	app: {
+		head: {
+			title: '',
+			meta: [
+				{charset: 'utf-8'},
+				{name: 'viewport', content: 'width=device-width, initial-scale=1'},
+				{hid: 'description', name: 'description', content: ''},
+				{name: 'format-detection', content: 'telephone=no'},
+			],
+			link: [
+				{rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
+			]
+		}
+	}
+})
