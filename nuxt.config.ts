b753de4003144aff8e1af866f438f779efe0591e
Brian Purgert <brianpurgert2@gmail.com>
2023-06-10 18:43:56 -0400

    .

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,15 @@
-// @ts-ignore
-import vuetify from 'vite-plugin-vuetify'
-
 export default defineNuxtConfig({
-		typescript: {shim: false}, build: {transpile: ['vuetify']}, sourcemap: {
-				client: false, server: false
-		}, modules: ['@kevinmarrec/nuxt-pwa', async (options, nuxt) => {
-				// @ts-ignore
-				nuxt.hooks.hook('vite:extendConfig', (config) => {
-						// @ts-ignore
-						return config.plugins.push(vuetify({
-								styles: {configFile: 'assets/variables.scss'}
-
-						}))
-				})
-		}], vite: {
-				define: {
-						'process.env.DEBUG': false
-				}
+	typescript: { shim: false }, build: { transpile: ['vuetify'] }, sourcemap: {
+		client: false, server: false
+	}, modules: ['@nuxt-alt/vuetify', '@kevinmarrec/nuxt-pwa'], vuetify: {
+		pluginOptions: { styles: true }, vuetifyOptions: {
+			blueprint: 'md3', theme: {
+				defaultTheme: 'light'
+			}
 		}
+	}, vite: {
+		define: {
+			'process.env.DEBUG': false
+		}
+	}
 })
